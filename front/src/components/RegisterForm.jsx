d12932fc59d8bd7d7817377a2b898f0aaf21d7e4
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2024-04-23 17:19:23 -0400

    Add JWT to API and FRONT

--- front/src/components/RegisterForm.jsx
+++ front/src/components/RegisterForm.jsx
@@ -7,17 +7,17 @@ const RegisterForm = () => {
 
     return (
         <div className="w-50 mx-auto my-5">
-            <form>
+            <form onSubmit={actions.handleRegister}>
                 <div className="form-group mb-3">
                     <label htmlFor="register_email" className="form-label">Email:</label>
-                    <input type="email" className="form-control" id="register_email" name="email" placeholder='user@domain.ext' />
+                    <input type="email" className="form-control" id="register_email" name="email" placeholder='user@domain.ext' value={store?.email} onChange={actions.handleChange} />
                 </div>
                 <div className="form-group mb-3">
                     <label htmlFor="register_password" className="form-label">Password:</label>
-                    <input type="password" className="form-control" id="register_password" name="password" placeholder='********' />
+                    <input type="password" className="form-control" id="register_password" name="password" placeholder='********' value={store?.password} onChange={actions.handleChange} />
                 </div>
                 <button className="btn btn-sm btn-primary w-100">
-                    Login
+                    Register
                 </button>
             </form>
         </div>
--- front/src/store/flux.js
+++ front/src/store/flux.js
@@ -4,7 +4,7 @@ const getState = ({ getStore, getActions, setStore }) => {
     return {
         store: {
             url: 'http://127.0.0.1:5000',
-            email: 'lrodriguez@4geeks.co',
+            email: '',
             password: '',
             current_user: null,
             access_token: null,
@@ -24,7 +24,13 @@ const getState = ({ getStore, getActions, setStore }) => {
                 login({ email, password })
 
             },
-            handleRegister: () => { },
+            handleRegister: (e) => {
+                e.preventDefault()
+                const { email, password } = getStore();
+                const { register } = getActions()
+
+                register({ email, password })
+            },
             checkCurrentUser: () => {
                 if (sessionStorage.getItem('access_token')) {
                     setStore({
@@ -68,7 +74,41 @@ const getState = ({ getStore, getActions, setStore }) => {
                 }
 
             },
-            register: () => { },
+            register: async (credenciales) => {
+                try {
+                    const { url } = getStore()
+                    const option = {
+                        method: 'POST',
+                        body: JSON.stringify(credenciales),
+                        headers: {
+                            'Content-type': 'application/json'
+                        }
+                    }
+
+                    const response = await fetch(`${url}/api/register`, option)
+                    const datos = await response.json()
+
+                    if (datos.msg) {
+                        console.log(datos)
+                        toast.error(datos.msg)
+                    } else {
+                        console.log(datos)
+                        const { access_token, user } = datos
+                        setStore({
+                            access_token: access_token,
+                            current_user: user,
+                            email: '',
+                            password: ''
+                        })
+                        sessionStorage.setItem('access_token', access_token)
+                        sessionStorage.setItem('current_user', JSON.stringify(user))
+                    }
+
+                } catch (error) {
+                    console.log(error.message)
+                }
+
+            },
             logout: () => {
                 if (sessionStorage.getItem('access_token')) {
                     setStore({
